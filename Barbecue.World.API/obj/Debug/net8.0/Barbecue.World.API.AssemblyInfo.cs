705ad239a7d8b97875fc98017a5415ca535327e0
samysdz <samysdz@gmail.com>
2024-03-19 22:04:43 -0300

    service file created

--- Barbecue.World.API/obj/Debug/net8.0/Barbecue.World.API.AssemblyInfo.cs
+++ Barbecue.World.API/obj/Debug/net8.0/Barbecue.World.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barbecue.World.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a4bd8dee309b6d06f31d964aa7291b7afeeceaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de575681b3d284271de42189f89b0bbcd4aa82ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barbecue.World.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barbecue.World.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Barbecue.World.Services/Meal/MealService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Barbecue.World.Services.Meal
+{
+    public class MealService
+    {
+    }
+}
